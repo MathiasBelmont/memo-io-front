1bdec638b1590a279505fb58730160884664f77b
Isaque <crisperohman@gmail.com>
2024-11-28 20:47:19 +0000

    MOD: NoteProps agora corresponde ao modelo do BD

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -2,35 +2,29 @@ import NoteModal from "./NoteModal";
 
 interface NoteProps {
   id: number;
+  createdAt: string;
   title: string;
-  note: string;
-  image?: string;
-  alt: string;
+  content: string;
   color: string;
 }
 
-export default function NoteCard({ id, title, note, image, alt, color }: NoteProps) {
+export default function NoteCard(props: NoteProps) {
 
   return (
     <>
-      <label htmlFor={`note-modal-${id}`} className="size-min">
+      <label htmlFor={`note-modal-${props.id}`} className="size-min">
         <div className="card size-80 drop-shadow-lg cursor-pointer select-none overflow-hidden">
           <div className="transition-opacity duration-300 hover:opacity-75 size-full">
             <div className="card-body select-none h-3/5 bg-yellow-100 text-gray-800">
-              <h2 className="card-title text-md">{title}</h2>
+              <h2 className="card-title text-md">{props.title}</h2>
               <p className="text-sm bg-gradient-to-b from-black to-transparent bg-clip-text text-transparent overflow-hidden">
-                {note}
+                {props.content}
               </p>
             </div>
-            {image && (
-              <figure className="overflow-hidden h-2/5">
-                <img src={image} alt={alt} />
-              </figure>
-            )}
           </div>
         </div>
       </label>
-      <NoteModal id={id} title={title} note={note} image={image} alt={alt} color={color} />
+      <NoteModal id={props.id} createdAt={props.createdAt} title={props.title} content={props.content} color={props.color} />
     </>
   );
 }
--- src/components/NoteModal.tsx
+++ src/components/NoteModal.tsx
@@ -1,25 +1,23 @@
 interface NoteProps {
   id: number;
+  createdAt: string;
   title: string;
-  note: string;
-  image?: string;
-  alt: string;
+  content: string;
   color: string;
 }
 
-export default function NoteModal({ id, title, note, image, alt, color }: NoteProps) {
+export default function NoteModal(props: NoteProps) {
   return (
     <>
-      <input type="checkbox" id={`note-modal-${id}`} className="modal-toggle" />
+      <input type="checkbox" id={`note-modal-${props.id}`} className="modal-toggle" />
       <div className="modal" role="dialog">
         <div className="modal-box bg-yellow-100 relative size-[800px] overflow-hidden">
-          <img src={image} alt={alt} className="absolute inset-0 w-full h-40 object-cover opacity-100" />
           <div className="pt-36 text-gray-800">
-            <h1 className="card-title text-xl">{title}</h1>
-            <p className="py-4">{note}</p>
+            <h1 className="card-title text-xl">{props.title}</h1>
+            <p className="py-4">{props.content}</p>
           </div>
         </div>
-        <label className="modal-backdrop" htmlFor={`note-modal-${id}`}/>
+        <label className="modal-backdrop" htmlFor={`note-modal-${props.id}`}/>
       </div>
     </>
   );
