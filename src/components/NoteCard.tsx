10defc9c11ee007c44fe951eb0a9e86ba7bf5c87
Isaque <crisperohman@gmail.com>
2024-11-29 19:40:43 +0000

    MOD: 'NoteCard' agora possui a cor correta e exibe a data e hora de criação da nota

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import NoteModal from "./NoteModal";
 
 interface NoteProps {
@@ -9,17 +10,55 @@ interface NoteProps {
 }
 
 export default function NoteCard(props: NoteProps) {
+  const [color, setColor] = useState(
+    props.color === "yellow"
+      ? `bg-yellow-100`
+      : props.color === "red"
+        ? `bg-red-100`
+        : props.color === "blue"
+          ? `bg-blue-100`
+          : props.color === "green"
+            ? `bg-green-100`
+            : `bg-gray-100`
+  );
+
+  useEffect(() => {
+    const newColor =
+      props.color === "yellow"
+        ? `bg-yellow-100`
+        : props.color === "red"
+          ? `bg-red-100`
+          : props.color === "blue"
+            ? `bg-blue-100`
+            : props.color === "green"
+              ? `bg-green-100`
+              : `bg-gray-100`;
+    if (newColor !== color) {
+      setColor(newColor);
+    }
+  }, [props.color, color]);
 
   return (
     <>
-      <label htmlFor={`note-modal-${props.id}`} className="size-min">
-        <div className="card size-80 drop-shadow-lg cursor-pointer select-none overflow-hidden">
-          <div className="transition-opacity duration-300 hover:opacity-75 size-full">
-            <div className="card-body select-none h-3/5 bg-yellow-100 text-gray-800">
-              <h2 className="card-title text-md">{props.title}</h2>
-              <p className="text-sm bg-gradient-to-b from-black to-transparent bg-clip-text text-transparent overflow-hidden">
-                {props.content}
-              </p>
+      <label htmlFor={`note-modal-${props.id}`} className="">
+        <div className={`card drop-shadow-lg cursor-pointer select-none`}>
+          <div className="transition-opacity duration-300 hover:opacity-50">
+            <div className={`card-body size-[250px] ${color} select-none text-gray-800`}>
+              <div className="flex flex-col h-[220px] gap-2 overflow-hidden overflow-wrap">
+                <h2 className="card-title text-md">{props.title}</h2>
+                <p className="text-xs flex-grow-0">
+                  {new Intl.DateTimeFormat("pt-BR", {
+                    day: "numeric",
+                    month: "long",
+                    year: "numeric",
+                    hour: "numeric",
+                    minute: "numeric"
+                  }).format(new Date(props.createdAt))}
+                </p>
+                <p className="text-sm h-0 bg-gradient-to-b from-black to-transparent bg-clip-text text-transparent">
+                  {props.content}
+                </p>
+              </div>
             </div>
           </div>
         </div>
