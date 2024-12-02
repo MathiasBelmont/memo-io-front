2c6b3a29a94573c94f70694b3ec33fe0c1f51e70
Isaque <crisperohman@gmail.com>
2024-12-02 13:05:35 +0000

    ADD: Comentários em 'Home'

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,37 +7,39 @@ import NotesAPI from "../utils/NotesAPI";
 export default function Home() {
   const navigate = useNavigate();
   const [notes, setNotes] = useState<any[]>([]);
+  const [theme, setTheme] = useState("light");
 
+  // Função para obter as notas do backend
   const fetchNotes = async () => {
     try {
       const data = await NotesAPI.getAll();
       if (data) {
         setNotes(data);
-      }          
+      }
     } catch (error) {
       console.error('Error fetching notes:', error);
     }
   };
-  
+
+  // UseEffect para buscar atualizações das notas periodicamente
   useEffect(() => {
     fetchNotes();
     const intervalId = setInterval(fetchNotes, 500);
     return () => clearInterval(intervalId);
   }, []);
 
+  // Função para sair da conta
   const handleLogout = () => {
     navigate("/");
   };
 
+  // Função para adicionar uma nova nota
   const handleAddNote = async () => {
     try {
       const noteData = {
-        title: "New Note",
-        content: Array(500)
-          .fill(null)
-          .map(() => String.fromCharCode(Math.floor(Math.random() * 26) + 97))
-          .join(" "),
-        color: ["yellow", "red", "blue", "green"][Math.floor(Math.random() * 4)],
+        title: "Nova nota",
+        content: "",
+        color: "bg-yellow-100",
         authorId: 1,
       };
       const response = await NotesAPI.create(noteData);
@@ -49,17 +51,20 @@ export default function Home() {
     }
   };
 
-  const [theme, setTheme] = useState("light");
+  // Função para alternar o tema
   const toggleTheme = () => {
     setTheme(theme === "dark" ? "light" : "dark");
   };
 
+  // UseEffect para atualizar o tema
   useEffect(() => {
     document.querySelector("html")?.setAttribute("data-theme", theme);
   }, [theme]);
 
   return (
     <>
+
+      {/* Barra de navegação */}
       <div className="navbar bg-base-300 fixed top-0 left-0 right-0 z-50 drop-shadow-lg">
         <div className="navbar-start">
           <button onClick={handleLogout} className="btn btn-ghost btn-circle">
@@ -75,6 +80,8 @@ export default function Home() {
           </button>
         </div>
       </div>
+      
+      {/* Notas */}
       <div className="flex justify-center items-center pt-16">
         <div className="grid grid-cols-3 gap-10 p-10">
           {notes.map((note) => (
@@ -89,12 +96,15 @@ export default function Home() {
           ))}
         </div>
       </div>
+      
+      {/* Botão para adicionar uma nova nota */}
       <button
         onClick={handleAddNote}
         className="btn bg-base-200 flex fixed bottom-10 right-10 shadow-md btn-lg rounded-full justify-center items-center"
       >
         <FaPencil />
       </button>
+
     </>
   );
 }
